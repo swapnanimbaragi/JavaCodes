045d663ca65d25dfd03f8da0dbb3166eba528af6
Swapna <swapnanimbaragi@gmail.com>
2026-02-01 19:36:38 +0530

    pgm4

---
+++ Pgm4.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+public class Pgm4 {
+    public static void main(String...args)
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(i==0 || i==4 || j==0 || j==4)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
