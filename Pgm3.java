d582b152bd0c05c824547c31d9a648670e075385
Swapna <swapnanimbaragi@gmail.com>
2026-02-01 19:11:54 +0530

    pgm 3

---
+++ Pgm3.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Pgm3 {
+    public static void main(String...args)
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
