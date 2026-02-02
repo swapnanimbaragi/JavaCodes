f0c7fe840608d15f82961d7848c53647a1c0cfd7
Swapna <swapnanimbaragi@gmail.com>
2026-02-02 19:05:09 +0530

    Pgm7

---
+++ Pgm7.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+public class Pgm7 
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+        int count;
+        for(int i=0;i<n;i++)
+        {
+            count=i+1;
+            for (int j=0;j<n;j++)
+            {
+              System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
