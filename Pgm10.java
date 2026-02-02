341a49fe95196947e389f49eb7fec846ad03d392
Swapna <swapnanimbaragi@gmail.com>
2026-02-02 19:17:49 +0530

    Pgm10

---
+++ Pgm10.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class Pgm10 
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for (int j=0;j<=i;j++)
+            {
+                System.out.print(i+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
