f10a152902ea826f2249ce841b93ed73ed4f21b4
Swapna <swapnanimbaragi@gmail.com>
2026-02-03 20:16:52 +0530

    holoeqtr

--- Pgm11.java
+++ Pgm11.java
@@ -19,7 +19,7 @@ public class Pgm11 {
         
     }
 
-       static void equilateraltringle()
+    static void equilateraltringle()
     {
         int n=5;
         for(int i=0;i<n;i++)
@@ -37,11 +37,37 @@ public class Pgm11 {
         
     }
     
+    static void equilateralHtringle()
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for(int k=0; k<(n-1)-i; k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0 || j==i || i==(n-1))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
 
     public static void main(String[] args) {
         System.out.println();
         reversetringle(); 
         System.out.println(); 
-        equilateraltringle();  
+        equilateraltringle(); 
+        System.out.println(); 
+        equilateralHtringle();
     }
 }
