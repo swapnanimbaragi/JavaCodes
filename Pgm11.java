71a55ee3c5d98c1775cee7343e1f637d7a604c59
Swapna <swapnanimbaragi@gmail.com>
2026-02-03 19:29:09 +0530

    trainglePgms

--- Pgm11.java
+++ Pgm11.java
@@ -1,7 +1,8 @@
 package Patterns;
 
 public class Pgm11 {
-    public static void main(String[] args) {
+    static void reversetringle()
+    {
         int n=5;
         for(int i=0;i<n;i++)
         {
@@ -11,10 +12,36 @@ public class Pgm11 {
             }
             for(int j=0;j<=i;j++)
             {
-                System.out.print("*" );
+                System.out.print("*");
             }
         System.out.println();
         }
         
+    }
+
+       static void equilateraltringle()
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for(int k=0;k<(n-1)-i;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* " );
+            }
+        System.out.println();
+        }
+        
+    }
+    
+
+    public static void main(String[] args) {
+        System.out.println();
+        reversetringle(); 
+        System.out.println(); 
+        equilateraltringle();  
     }
 }
