d8351c6b1411d57647ee617f855186c789392a5a
Swapna <swapnanimbaragi@gmail.com>
2026-02-03 21:55:47 +0530

    trainglePgms

--- Pgm11.java
+++ Pgm11.java
@@ -103,6 +103,24 @@ public class Pgm11 {
                 System.out.println(); 
             }
         }
+
+        static void numCtringle()
+        {
+            int n=5;
+            
+            for(int i=0;i<=n;i++)
+            {
+                int count=i;
+                for(int j=0;j<i;j++)
+                {
+                    System.out.print(count++ +  " ");
+                }
+                System.out.println();
+            }
+        }
+        
+
+
     
 
         
@@ -117,5 +135,7 @@ public class Pgm11 {
         numequltraltringle();
         System.out.println(); 
         numHtringle();
+        System.out.println();
+        numCtringle();
     }
 }
