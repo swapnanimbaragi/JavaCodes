cd2622f2de30666fdc275b585f0999a2b939c4b9
Swapna <swapnanimbaragi@gmail.com>
2026-02-05 20:07:32 +0530

    tringlepgms

--- Pgm11.java
+++ Pgm11.java
@@ -278,6 +278,30 @@ public class Pgm11 {
             System.out.println();
         }
     }
+
+    static void numperaincrdecr()
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            int count=1;
+            for(int k=1;k<=(n-i);k++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(2*i)-1;j++)
+            {
+                if(j<i)
+                {
+                    System.out.print(count++ + " ");
+                }
+                else{
+                    System.out.print(count-- + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
     
 
     
@@ -314,6 +338,8 @@ public class Pgm11 {
         Holocode();
         System.out.println();
         numincrdecr();
+        System.out.println();
+        numperaincrdecr();
 
     }
 }
