ed464538669e73fab68c3b434ffb5e5da5b80bbd
Swapna <swapnanimbaragi@gmail.com>
2026-02-05 17:46:35 +0530

    tringlepgms

--- Pgm11.java
+++ Pgm11.java
@@ -187,11 +187,11 @@ public class Pgm11 {
         {
             for(int k=1;k<=(n-i);k++)
             {
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for(int j=1;j<=(2*i)-1; j++)
             {
-                System.out.print("*");
+                System.out.print("*" + " ");
             }
                
             System.out.println();
