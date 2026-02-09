45d92d0eb5ca2005e720aedb7c0b92be6a2e84fd
Swapna <swapnanimbaragi@gmail.com>
2026-02-09 22:33:46 +0530

    datatypes

--- datatypes/Add.java
+++ datatypes/Add.java
@@ -9,10 +9,25 @@ public class Add {
         int b=scan.nextInt();
         System.out.println(a+b);
     }
+
+    static void Product()
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a and b and c value");
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        System.out.println(a*b*c);
+
+    }
+
+
     public static void main(String[] args) {
         {
             System.out.println();
             Sum();
+            System.out.println();
+            Product();
         }
     }
 }
