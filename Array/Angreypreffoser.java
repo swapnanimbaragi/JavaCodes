336ed2201b2a578e8270815e0784db10dde20a3a
Swapna <swapnanimbaragi@gmail.com>
2026-02-10 20:29:55 +0530

    MathProblems

---
+++ Array/Angreypreffoser.java
@@ -0,0 +1,13 @@
+package Patterns.Array;
+
+public class Angreypreffoser{
+
+    int coount=0;
+    for(int i=1;i<=a.length;i++)
+    {
+        if(arr[]<=i)
+        {
+            
+        }
+    }
+}
---
+++ math/Counter.java
@@ -0,0 +1,24 @@
+package Patterns.math;
+
+import java.util.Scanner;
+
+public class Counter 
+{
+    static int countofdigit(int n)
+    {
+        int count=0;
+        while(n!=0)
+        {
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scan= new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.println(countofdigit(n));
+    }
+}
