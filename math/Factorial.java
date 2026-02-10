e642e7e5d5d3685d9f8152c2c44486a9f2a7d01d
Swapna <swapnanimbaragi@gmail.com>
2026-02-10 20:50:03 +0530

    MathProblems

---
+++ math/Factorial.java
@@ -0,0 +1,22 @@
+package Patterns.math;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int fact(int n)
+    {
+        int result=1;
+        for(int i=1;i<=n;i++)
+        {
+            result =(result * i);
+        }
+        return result;
+
+    }
+    public static void main(String...args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.println(fact(n));
+    }
+}
