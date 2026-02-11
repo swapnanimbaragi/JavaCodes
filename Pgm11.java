7feb8e39bc439001531c8f0b3e987d2bfab5f61c
Swapna <swapnanimbaragi@gmail.com>
2026-02-11 23:09:05 +0530

    Patterns

--- Pgm11.java
+++ Pgm11.java
@@ -611,11 +611,11 @@ public class Pgm11 {
         {
             for(int k=1;k<=(n-i);k++)
             {
-                System.out.print("$");
+                System.out.print("$ ");
             }
             for(int j=1;j<=i;j++)
             {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
