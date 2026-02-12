4b212df6509c6f8287206ffebd6d6494f810cf3f
Swapna <swapnanimbaragi@gmail.com>
2026-02-12 22:47:45 +0530

    datatypes

--- datatypes/Byte.java
+++ datatypes/Byte.java
@@ -3,15 +3,26 @@ package Patterns.datatypes;
 public class Byte{
     public static void main(String...args)
     {
-    byte a=127;
-    a++;
-    System.out.println(a);
+    // byte a=127;
+    // a++;
+    // System.out.println(a);
 
-    int b=10;
-    System.out.println(b++ + ++b);
-    int c=5;
-    int d=10;
-    System.out.println(c & d);
+    // int b=10;
+    // System.out.println(b++ + ++b);
+    // int c=5;
+    // int d=10;
+    // System.out.println(c & d);
+
+    // byte b=10;
+    // b=(byte)(b*2);
+    // double d=10/4;
+    // System.out.println(d);
+    // int a=5;
+    // a*=3+2;
+    // System.out.println(a);
+        char a='A';
+        a++;
+        System.out.println(a);
     }
 
 }
