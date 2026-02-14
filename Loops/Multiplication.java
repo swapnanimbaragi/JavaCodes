b3bd381c6798e4c3cbcd37bfef8fcdc83d2cfcea
Swapna <swapnanimbaragi@gmail.com>
2026-02-14 23:34:09 +0530

    Loops

--- Loops/Multiplication.java
+++ Loops/Multiplication.java
@@ -5,7 +5,7 @@ public class Multiplication {
     public static void main(String... args)
     {
         int n=10;
-        // int num=5;
+        
         Scanner scan=new Scanner(System.in);
         int num=scan.nextInt(); 
         for(int i=1;i<=n;i++)
