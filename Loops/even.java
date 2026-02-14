c104d3277503c594eb8d0835f88c0164f8577f39
Swapna <swapnanimbaragi@gmail.com>
2026-02-14 23:09:36 +0530

    Loops

---
+++ Loops/even.java
@@ -0,0 +1,15 @@
+package Patterns.Loops;
+
+public class even {
+    public static void main(String... args)
+    {
+        int n=10;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
