dddff006081b8c5e3caccb3c7eb0e6404c64e9ec
Swapna <swapnanimbaragi@gmail.com>
2026-02-14 23:32:18 +0530

    loops

--- Loops/Sumofdigits.java
+++ Loops/Sumofdigits.java
@@ -7,9 +7,6 @@ public class Sumofdigits {
     {
         int n=10;
         int sum=0;
-        // int num=5;
-        // Scanner scan=new Scanner(System.in);
-        // int num=scan.nextInt(); 
         for(int i=1;i<=n;i++)
         {
             sum=sum+i;
