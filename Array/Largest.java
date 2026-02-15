2b25383b773dfef0778e08bd9598aa4711b12e4a
Swapna <swapnanimbaragi@gmail.com>
2026-02-15 15:49:19 +0530

    Array

---
+++ Array/Largest.java
@@ -0,0 +1,34 @@
+package Patterns.Array;
+
+import java.util.Scanner;
+
+public class Largest {
+    
+    public static void main(String[] args)
+    {
+        
+        Scanner scan=new Scanner(System.in);
+        int a[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("enter the ages" + i);
+            a[i]=scan.nextInt();
+        }
+        System.out.println("Ages");
+        int max=a[0];
+        for(int i=0;i<5;i++)
+        {
+            
+            if(a[i]>max)
+            {
+                max=a[i];
+                
+            }
+            
+        }
+        System.out.println(max);
+        
+    
+    }
+}
+
