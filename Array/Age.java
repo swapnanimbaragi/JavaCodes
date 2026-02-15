1d1c076681917a407d11922b91ca4f19710d578b
Swapna <swapnanimbaragi@gmail.com>
2026-02-15 15:22:44 +0530

    Array

--- Array/Age.java
+++ Array/Age.java
@@ -2,9 +2,29 @@ package Patterns.Array;
 
 import java.util.Scanner;
 
-public class Age {
+// public class Age {
+//     public static void main(String[] args)
+//     {
+//         Scanner scan=new Scanner(System.in);
+//         int a[]=new int[5];
+//         for(int i=0;i<5;i++)
+//         {
+//             System.out.println("enter the ages" + i);
+//             a[i]=scan.nextInt();
+//         }
+//         System.out.println("Ages");
+//         for(int i=0;i<5;i++)
+//         {
+//             a[3]=60;
+//             System.out.println(a[i]);
+//         }
+//     }
+    
+
+    public class Age {
     public static void main(String[] args)
     {
+        int sum=0;
         Scanner scan=new Scanner(System.in);
         int a[]=new int[5];
         for(int i=0;i<5;i++)
@@ -15,9 +35,9 @@ public class Age {
         System.out.println("Ages");
         for(int i=0;i<5;i++)
         {
-            a[3]=60;
-            System.out.println(a[i]);
+            sum=sum+a[i];
+            
         }
+        System.out.println(sum);
     }
-    
 }
