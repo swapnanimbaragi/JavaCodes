fb196de6fdb9cba4269d4b9e85591881b95863fe
Swapna <swapnanimbaragi@gmail.com>
2026-02-16 20:36:00 +0530

    Array

--- Array/Evenindex.java
+++ Array/Evenindex.java
@@ -15,6 +15,7 @@ public class Evenindex
         
         for(int i=0;i<5;i++)
         {
+            // if(a[i]%2==0)
             if(i%2==0)
             {
                 System.out.println(a[i]);
