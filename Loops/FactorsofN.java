80a1db549b81b5a2c54d652ddd0f815f957e6554
Swapna <swapnanimbaragi@gmail.com>
2026-02-17 23:16:34 +0530

    Loops

--- Loops/FactorsofN.java
+++ Loops/FactorsofN.java
@@ -16,7 +16,6 @@ public class FactorsofN {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
-        
         factors(n);
     }
 
