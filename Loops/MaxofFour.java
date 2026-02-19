dc68f8b1b0456a2c1c2e3616ff16f8d62bb2d75d
Swapna <swapnanimbaragi@gmail.com>
2026-02-19 23:26:51 +0530

    Loops

---
+++ Loops/MaxofFour.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MaxofFour {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        int d=scan.nextInt();
+        int max1=a > b ? a : b;
+        int max2=c > d ? c : d;
+        int result=max1 > max2 ? max1 : max2;
+        System.out.println(result);
+    }
+
+
+}
