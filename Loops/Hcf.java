8e6276cda56d9fe3d52ed5162729e859845e8428
Swapna <swapnanimbaragi@gmail.com>
2026-02-20 22:19:33 +0530

    Loops

--- Loops/Hcf.java
+++ Loops/Hcf.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Hcf {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // System.out.println("Enter n and m value");
+        System.out.println("Enter n and m value");
         int n = scan.nextInt();
         int m = scan.nextInt();
         // implement your logic here
