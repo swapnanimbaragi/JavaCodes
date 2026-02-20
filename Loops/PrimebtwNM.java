6eefafa0be20e40dd4227f5553de4db55a735946
Swapna <swapnanimbaragi@gmail.com>
2026-02-20 21:58:56 +0530

    Loops

--- Loops/PrimebtwNM.java
+++ Loops/PrimebtwNM.java
@@ -19,10 +19,8 @@ public class PrimebtwNM
         {
             return false;
         }
-        // string result=checkprime(n);
-        // System.out.println(result);
+        
     }
-
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
