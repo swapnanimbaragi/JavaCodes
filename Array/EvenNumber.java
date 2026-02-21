ae2a365a685cf2f5f18b38db13297f9046febc85
Swapna <swapnanimbaragi@gmail.com>
2026-02-21 22:58:53 +0530

    Loops

---
+++ Array/EvenNumber.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class EvenNumber {
+    static boolean evennum(int n)
+    {
+        if(n%2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scan.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(evennum(a[i]))
+            {
+                System.out.println(a[i]);
+            }
+            
+        }
+    }
+}
