ea7f8959e04a27377dcc790a517e37c085dd5eeb
Swapna <swapnanimbaragi@gmail.com>
2026-02-21 23:26:59 +0530

    Array

--- Array/SmallestNum.java
+++ Array/SmallestNum.java
@@ -4,12 +4,14 @@ public class SmallestNum {
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter Size of Array");
         int n=scan.nextInt();
+        System.out.println("Enter Numbers");
         int a[]=new int[n];
         for(int i=0;i<n;i++)
         {
             a[i]=scan.nextInt();
         }
         int min=a[0];
+        System.out.println("Minimum Number:");
         for(int i=0;i<n;i++)
         {
             if(a[i]<min)
