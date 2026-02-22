6847f311deabed6ef6ab8769326a0534f801e643
Swapna <swapnanimbaragi@gmail.com>
2026-02-22 18:44:37 +0530

    Array

--- Array/ElementFStoMidd.java
+++ Array/ElementFStoMidd.java
@@ -5,16 +5,16 @@ public class ElementFStoMidd
     public static void main(String[] args)
     {
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter size of array");
         int n=scan.nextInt();
         int a[]=new int[n];
         for(int i=0;i<n;i++)
         {
             a[i]=scan.nextInt();
         }
-        // int mid=0;
+        
         for(int i=0;i<n/2;i++)
         {
-            // mid=i%2==0;
             System.out.println(a[i]);
         }
     }
