5167ec690806056ae2775cdcfbe72ceb33b90b1e
Swapna <swapnanimbaragi@gmail.com>
2026-02-22 19:26:50 +0530

    Array

--- Array/SumofmidElem.java
+++ Array/SumofmidElem.java
@@ -4,8 +4,10 @@ public class SumofmidElem
     public static void main(String[] args) 
     {
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter Array Size");
         int n=scan.nextInt();
         int a[]=new int[n];
+        System.out.println("Enter Array Elemnets");
         for(int i=0;i<n;i++)
         {
             a[i]=scan.nextInt();
@@ -15,6 +17,6 @@ public class SumofmidElem
         {
             sum=sum+a[i];
         }
-        System.out.println(sum);
+        System.out.println("SumofmidE="+ sum);
     }    
 }
