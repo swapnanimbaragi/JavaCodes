53ef80d41f356741fcb368195c23cac9f56aac59
Swapna <swapnanimbaragi@gmail.com>
2026-02-24 23:05:14 +0530

    Array

--- Array/Main.java
+++ Array/Main.java
@@ -2,12 +2,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
         int N = sc.nextInt();
         int a[]=new int[N];
         for(int i=0;i<N;i++)
         {
             a[i]=sc.nextInt();
         }
+
+
+
         int largest=Integer.MIN_VALUE;
         int count=0;
         for(int i=0;i<N;i++)
