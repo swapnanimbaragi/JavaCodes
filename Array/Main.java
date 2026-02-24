26776baf8b3faa8249e13192aaca850ba5f9d6ba
Swapna <swapnanimbaragi@gmail.com>
2026-02-24 22:55:21 +0530

    Array

--- Array/Main.java
+++ Array/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        // implement your logic here
         int a[]=new int[N];
         for(int i=0;i<N;i++)
         {
