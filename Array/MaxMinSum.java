2183b6b7da36d1d10293dc7cb44146bce26aefe2
Swapna <swapnanimbaragi@gmail.com>
2026-02-25 21:35:35 +0530

    Array

--- Array/MaxMinSum.java
+++ Array/MaxMinSum.java
@@ -39,6 +39,7 @@ public class MaxMinSum
 
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
+        System.out.println("Enter Array size");
 		int n=scan.nextInt();
 		int a[]=new int[n];
 		for(int i=0;i<a.length;i++)
