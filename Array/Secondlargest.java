7ec640ec9cc0b7620f71c8d92bedff686b49e008
Swapna <swapnanimbaragi@gmail.com>
2026-02-26 22:20:55 +0530

    Array

--- Array/Secondlargest.java
+++ Array/Secondlargest.java
@@ -2,14 +2,29 @@ import java.util.*;
 public class Secondlargest {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        int n=0;
-        int a[]=new int[n];
         System.out.println("enter array");
+        int n=scan.nextInt();
+        int a[]=new int[n];
+        
         for(int i=0;i<n ;i++)
         {
             a[i]=scan.nextInt();
         }
         System.out.println("second largest");
-        
+        int p=Integer.MIN_VALUE;
+        int vp=Integer.MIN_VALUE;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]>p)
+            {
+                vp=p;
+                p=a[i];
+            }
+            else if(p>vp)
+            {
+                vp=a[i];
+            }
+        }
+        System.out.print(vp);
     }
 }
