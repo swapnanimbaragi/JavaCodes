d931dd62cf2ee80cc4b64ae1e98486e883dfd9e9
Swapna <swapnanimbaragi@gmail.com>
2026-02-26 22:08:26 +0530

    Array

---
+++ Array/PosNeg.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PosNeg {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        // implement your logic here
+        int a[]=new int[N];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]<0)
+            {
+                System.out.print(a[i] + " ");
+            }
+        }
+        System.out.println();
+        for(int k=0;k<a.length;k++)
+        {
+            if(a[k]%2 !=0 && a[k]>0)
+            {
+                System.out.print(a[k] + " ");
+            }
+        }
+        System.out.println();
+        for(int j=0;j<a.length;j++)
+        {
+            if(a[j]%2 == 0 && a[j] >0)
+            {
+                System.out.print(a[j] + " ");
+            }
+        }
+            
+        
+    }
+}
