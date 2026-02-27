5dcc0f418b29045c30464d05ce8e5a46a3709ae9
Swapna <swapnanimbaragi@gmail.com>
2026-02-27 14:30:59 +0530

    Array

--- Array/Reversearray.java
+++ Array/Reversearray.java
@@ -4,7 +4,6 @@ public class Reversearray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        // implement your logic here
         int a[]=new int[N];
         for(int i=0;i<a.length;i++)
         {
