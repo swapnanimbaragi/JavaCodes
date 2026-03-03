79fc7ccf7de7d97ff045932fe5e785d9b9fa8c19
Swapna <swapnanimbaragi@gmail.com>
2026-03-03 21:41:28 +0530

    Array

---
+++ Array/firstsmallerPair.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class firstsmallerPair {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+       
+        for(int i=0;i<a.length-1;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                if(a[i]<a[j])
+                {
+                    System.out.println(a[i]+" "+a[j]);
+                }
+            }
+        }
+       
+    }
+}
