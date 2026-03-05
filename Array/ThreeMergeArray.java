cdb68d8d93d5d3cd8d20f9e3a47ebd67d8d44d6d
Swapna <swapnanimbaragi@gmail.com>
2026-03-05 22:15:24 +0530

    Array

--- Array/ThreeMergeArray.java
+++ Array/ThreeMergeArray.java
@@ -4,6 +4,7 @@ public class ThreeMergeArray {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the size of first array:");
         int n1=scanner.nextInt();
+        System.out.println("Enter the elements of first array:");
         int []a1=new int[n1];
         for(int i=0;i<a1.length;i++)
         {
