ee49bbd2a315e08f3c629486ff52a0a197c0af56
Swapna <swapnanimbaragi@gmail.com>
2026-03-06 21:32:25 +0530

    Array

--- Array/RearrangementArray.java
+++ Array/RearrangementArray.java
@@ -7,7 +7,7 @@ public class RearrangementArray {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
         int n = scanner.nextInt();
-
+        System.out.println("Enter the elements of the array:");
         int a[] = new int[n];
 
         for(int i = 0; i < n; i++)
