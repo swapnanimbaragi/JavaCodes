37da48f94574558b4fa729685d326a4503d9a033
Swapna <swapnanimbaragi@gmail.com>
2026-03-07 13:53:40 +0530

    Array

--- Array/EvenSubarray.java
+++ Array/EvenSubarray.java
@@ -4,7 +4,9 @@ public class EvenSubarray {
      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Your code here
+        System.out.print("Enter the size of the array: ");
         int n=scanner.nextInt();
+        System.out.println("Enter the elements of the array: ");
         int a[]=new int[n];
         for(int i=0;i<n;i++)
         {
