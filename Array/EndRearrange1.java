fc074f1039a95eb88bf92b6a0f313a7484cc54f3
Swapna <swapnanimbaragi@gmail.com>
2026-03-08 18:31:00 +0530

    Array

--- Array/EndRearrange1.java
+++ Array/EndRearrange1.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class EndRearrange1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
         int n=scanner.nextInt();
         int a[]=new int[n];
+        System.out.print("Enter the elements of the array: ");
         for(int i=0;i<n;i++)
         {
             a[i]=scanner.nextInt();
