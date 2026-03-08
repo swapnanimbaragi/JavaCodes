55c2f3ed090e7b2200e1541e7f2181387fa64d24
Swapna <swapnanimbaragi@gmail.com>
2026-03-08 18:29:52 +0530

    Array

---
+++ Array/EndRearrange1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EndRearrange1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int i=0;
+        int j=0;
+        while(i<a.length)
+        {
+            if(a[i]==-1)
+            {
+                i++;
+            }
+            else
+            {
+                a[j]=a[i];
+                i++;
+                j++;
+            }
+        }
+        while(j<a.length)
+        {
+            a[j]=-1;
+            j++;
+        }
+        for(int x=0;x<a.length;x++)
+        {
+            System.out.print(a[x]+" ");
+        }
+    
+    }
+}
