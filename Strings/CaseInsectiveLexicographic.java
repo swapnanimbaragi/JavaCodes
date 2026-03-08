970baee4ef3a9f3066ffc7e471a4475ddbefda11
Swapna <swapnanimbaragi@gmail.com>
2026-03-08 18:24:19 +0530

    String

---
+++ Strings/CaseInsectiveLexicographic.java
@@ -0,0 +1,22 @@
+package Strings;
+import java.util.Scanner;
+public class CaseInsectiveLexicographic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1=scanner.nextLine();
+        String s2=scanner.nextLine();
+        int result=s1.compareToIgnoreCase(s2);
+        if(result>0)
+        {
+            System.out.println("s1>s2");
+        }
+        else if(result<0)
+        {
+            System.out.println("s2<s1");
+        }
+        else
+        {
+            System.out.println("Both strings are equal");
+        }
+    }
+}
