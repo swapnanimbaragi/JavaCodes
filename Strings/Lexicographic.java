4ca3a8445288f4af3fcda15d69a6f9107e022ea0
Swapna <swapnanimbaragi@gmail.com>
2026-03-08 18:26:15 +0530

    String

--- Strings/Lexicographic.java
+++ Strings/Lexicographic.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Lexicographic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
         String s1=scanner.nextLine();
         String s2=scanner.nextLine();
         int result=s1.compareTo(s2);
