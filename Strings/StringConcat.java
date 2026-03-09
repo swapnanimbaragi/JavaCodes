1c9ad8081bd0a66f5aa43ece76b1d360168755c7
Swapna <swapnanimbaragi@gmail.com>
2026-03-09 22:49:26 +0530

    String

---
+++ Strings/StringConcat.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        String input2 = scanner.nextLine();
+        System.out.print(input1+input2);
+       
+    }
+}
