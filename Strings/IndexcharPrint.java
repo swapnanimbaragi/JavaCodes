955045d64080a852f216b72cea401b1b181a1187
Swapna <swapnanimbaragi@gmail.com>
2026-03-09 22:33:03 +0530

    String

--- Strings/IndexcharPrint.java
+++ Strings/IndexcharPrint.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class IndexcharPrint {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         int k=scanner.nextInt();
         if(input.isEmpty()||k<0 || k>=input.length())
