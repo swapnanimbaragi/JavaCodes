a69c80506ae752a9348011ac582eedb969b641ca
Swapna <swapnanimbaragi@gmail.com>
2026-03-09 22:32:18 +0530

    String

---
+++ Array/ConsecutiveSubArrayDecending.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ConsecutiveSubArrayDecending {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter the elements of the array: ");
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        for(int i=a.length-1;i>0;i--)
+        {
+            System.out.print(a[i] + " ");
+            if(a[i] - a[i+1] != 1)
+            {
+                System.out.println();
+            }
+        }
+        System.out.print(a[a.length-1]);
+    }
+}
---
+++ Strings/IndexcharPrint.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.Scanner;
+public class IndexcharPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int k=scanner.nextInt();
+        if(input.isEmpty()||k<0 || k>=input.length())
+        {
+            System.out.print("Invalid index");
+        }
+        else
+        {
+            System.out.print(input.charAt(k));
+        }
+    }
+}
