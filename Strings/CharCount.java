64d23b53bb929db597a8152a029c650c91467bc9
Swapna <swapnanimbaragi@gmail.com>
2026-03-09 22:47:42 +0530

    String

--- Strings/CharCount.java
+++ Strings/CharCount.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CharCount {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a string: ");
     String s=scanner.nextLine();
     System.out.print(s.length());
  }
