70902e05921891549cc61dfc11ae59dbe44a0fcc
Swapna <swapnanimbaragi@gmail.com>
2026-03-09 22:47:00 +0530

    String

---
+++ Strings/CharCount.java
@@ -0,0 +1,12 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CharCount {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String s=scanner.nextLine();
+    System.out.print(s.length());
+ }
+}
+
