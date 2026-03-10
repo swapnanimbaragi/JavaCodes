cf931ca59304ad4c9664ccc926a475b28ce448db
Swapna <swapnanimbaragi@gmail.com>
2026-03-10 18:54:42 +0530

    String

--- Strings/OddindexCharPrint.java
+++ Strings/OddindexCharPrint.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class OddindexCharPrint {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         boolean found=false;
         for(int i=0;i<=input.length()-1;i++)
