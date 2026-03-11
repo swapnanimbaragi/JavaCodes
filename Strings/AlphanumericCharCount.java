ab0af41c0bcc0ea8884c358541fae4e7ba962c6a
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:27:45 +0530

    String

---
+++ Strings/AlphanumericCharCount.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class AlphanumericCharCount {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s=scanner.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
+            {
+                count++;
+            }
+        }
+        System.out.print(count);
+        
+    }
+}
