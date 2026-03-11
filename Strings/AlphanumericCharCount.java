6504d4f457e423a07f519aeee9e39c7403157906
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:29:46 +0530

    String

--- Strings/AlphanumericCharCount.java
+++ Strings/AlphanumericCharCount.java
@@ -6,6 +6,7 @@ public class AlphanumericCharCount {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String s=scanner.nextLine();
         int count=0;
         for(int i=0;i<s.length();i++)
