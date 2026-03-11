cba6bc3c9b2a1c901ab93eb3cab37467ee0c38db
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:39:06 +0530

    String

--- Strings/SpecialCharPrinter.java
+++ Strings/SpecialCharPrinter.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SpecialCharPrinter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         boolean found=false;
         for(int i=0;i<input.length();i++)
