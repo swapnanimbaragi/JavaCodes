4f14a7a3c36d8cc6145ee86e5f73166d46409db2
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:45:08 +0530

    String

---
+++ Strings/countVowConsSChara.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class countVowConsSChara {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int vc=0, cc=0, sc=0;
+        for(int i=0;i<input.length();i++)
+        {
+            char ch=input.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
+            {
+                if((ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U' ||
+                ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'))
+                {
+                    vc++;
+                }
+                else
+                {
+                    cc++;
+                }
+
+            }
+            else if(!(ch>='0' && ch<='9'))
+            {
+                sc++;
+            }
+        }
+        System.out.println("Vowels: " + vc);
+        System.out.println("Consonants: " + cc);
+        System.out.print("Special Characters: " + sc);
+    }
+}
