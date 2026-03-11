ca2e87ee5549f063e3244ebb27d7d714ffdfe2bd
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:42:46 +0530

    String

--- Strings/ConsonantCountString.java
+++ Strings/ConsonantCountString.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ConsonantCountString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         int count=0;
         for(int i=0;i<input.length();i++)
