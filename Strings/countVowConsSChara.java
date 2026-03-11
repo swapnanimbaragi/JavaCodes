ad55273e38ea693bf366b34a596e4f5fdc2dd285
Swapna <swapnanimbaragi@gmail.com>
2026-03-11 20:45:51 +0530

    String

--- Strings/countVowConsSChara.java
+++ Strings/countVowConsSChara.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class countVowConsSChara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String input = scanner.nextLine();
         int vc=0, cc=0, sc=0;
         for(int i=0;i<input.length();i++)
