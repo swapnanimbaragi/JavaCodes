018ce867c0ad96847dd49a5b3cef06461e5e3bf0
Swapna <swapnanimbaragi@gmail.com>
2026-03-12 21:53:28 +0530

    String

--- Strings/UpperASCII.java
+++ Strings/UpperASCII.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class UpperASCII {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
         int n = scanner.nextInt();
         if(n>=65 && n<=90)
         {
