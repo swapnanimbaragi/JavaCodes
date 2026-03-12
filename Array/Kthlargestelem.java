3aeeb50ff95ff777cc9c226ba82e7fdc37cecf49
Swapna <swapnanimbaragi@gmail.com>
2026-03-12 18:26:57 +0530

    Array

---
+++ Array/Kthlargestelem.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Kthlargestelem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        int n=scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int k=scanner.nextInt();
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<k;i++)
+        {
+            largest=Integer.MIN_VALUE;
+            for(int j=0;j<n;j++)
+            {
+                if(a[j]>largest)
+                {
+                    largest=a[j];
+                }
+
+            }
+            for(int j=0;j<n;j++)
+            {
+                if(a[j]==largest)
+                {
+                    a[j]=Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        System.out.print(largest);
+    }
+}
+
--- Strings/countVowConsSChara.java
+++ Strings/countVowConsSChara.java
@@ -5,32 +5,41 @@ import java.util.Scanner;
 public class countVowConsSChara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        int vc=0, cc=0, sc=0;
-        for(int i=0;i<input.length();i++)
-        {
-            char ch=input.charAt(i);
-            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
-            {
-                if((ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U' ||
-                ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'))
-                {
-                    vc++;
-                }
-                else
-                {
-                    cc++;
-                }
+    //     System.out.print("Enter a string: ");
+    //     String input = scanner.nextLine();
+    //     int vc=0, cc=0, sc=0;
+    //     for(int i=0;i<input.length();i++)
+    //     {
+    //         char ch=input.charAt(i);
+    //         if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
+    //         {
+    //             if((ch=='A' || ch=='E' || ch=='I' || ch=='O' ||ch=='U' ||
+    //             ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'))
+    //             {
+    //                 vc++;
+    //             }
+    //             else
+    //             {
+    //                 cc++;
+    //             }
 
-            }
-            else if(!(ch>='0' && ch<='9'))
-            {
-                sc++;
-            }
+    //         }
+    //         else if(!(ch>='0' && ch<='9'))
+    //         {
+    //             sc++;
+    //         }
+    //     }
+    //     System.out.println("Vowels: " + vc);
+    //     System.out.println("Consonants: " + cc);
+    //     System.out.print("Special Characters: " + sc);
+    // }
+
+        int n=scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
         }
-        System.out.println("Vowels: " + vc);
-        System.out.println("Consonants: " + cc);
-        System.out.print("Special Characters: " + sc);
+        System.out.print((a.length)/2);
     }
 }
