615301c62f901bd302327d81b31ee758c8a1a801
Swapna <swapnanimbaragi@gmail.com>
2026-03-13 22:29:14 +0530

    Methodoverloading

--- MethodOverLoading/DivisionMOL.java
+++ MethodOverLoading/DivisionMOL.java
@@ -19,9 +19,9 @@ public class DivisionMOL {
     }
     public static void main(String[] args) {
         DivisionMOL obj = new DivisionMOL();
-        System.out.println(obj.divide(20, 10));
-        System.out.println(obj.divide(20.5f, 10.5f));
-        System.out.println(obj.divide(30.5, 20.5, 10.5));
-        System.out.println(obj.divide(20, 10.5));
+        System.out.println("Division of integers: " + obj.divide(20, 10));
+        System.out.println("Division of floats: " + obj.divide(20.5f, 10.5f));
+        System.out.println("Division of doubles: " + obj.divide(30.5, 20.5, 10.5));
+        System.out.println("Division of int and double: " + obj.divide(20, 10.5));
     }
 }
