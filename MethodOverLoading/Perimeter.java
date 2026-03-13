77d015de950b29bcc5e2f3893d53a2b6146a2fef
Swapna <swapnanimbaragi@gmail.com>
2026-03-13 22:23:38 +0530

    Methodoverloading

---
+++ MethodOverLoading/Perimeter.java
@@ -0,0 +1,27 @@
+package MethodOverLoading;
+
+public class Perimeter {
+    int perimeter(int a)
+    {
+        return 4*a;
+    }
+    double perimeter(double a, double b)
+    {
+        return 2*(a+b);
+    }
+    double perimeter(double r)
+    {
+        return 2 * Math.PI * r;
+    }
+    double perimeter(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+    public static void main(String[] args) {
+        Perimeter obj = new Perimeter();
+        System.out.println(obj.perimeter(20));
+        System.out.println(obj.perimeter(20.5, 10.5));
+        System.out.println(obj.perimeter(30.5));
+        System.out.println(obj.perimeter(20, 10, 15));
+    }
+}
