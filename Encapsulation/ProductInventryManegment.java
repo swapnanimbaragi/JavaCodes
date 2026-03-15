200d450e423ec38a5026252f5d65c38436fc31d7
Swapna <swapnanimbaragi@gmail.com>
2026-03-15 20:48:42 +0530

    Encapsulation

---
+++ Encapsulation/ProductInventryManegment.java
@@ -0,0 +1,33 @@
+package Encapsulation;
+
+public class ProductInventryManegment {
+    private String productName;
+    private String productId;       
+    private int quantity;
+    private double price;
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+}
