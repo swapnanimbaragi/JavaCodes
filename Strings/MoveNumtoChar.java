196b99773a8c887e71525975ca5d47dfc5919af7
Swapna <swapnanimbaragi@gmail.com>
2026-03-16 16:55:11 +0530

    String

---
+++ Strings/MoveNumtoChar.java
@@ -0,0 +1,27 @@
+package Strings;
+import java.util.*;
+public class MoveNumtoChar {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String letters="";
+        String number="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='0' && ch<='9')
+            {
+                number=number+ch;
+            }
+            else
+            {
+                letters=letters+ch;
+            }
+                
+            
+        }
+        System.out.print(letters+number);
+
+    }
+}
