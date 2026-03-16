5e8406cc6f24bfb902cb04f5e0dcb6b984a2636a
Swapna <swapnanimbaragi@gmail.com>
2026-03-16 12:45:33 +0530

    String

---
+++ Strings/LowertoUpperConvert.java
@@ -0,0 +1,22 @@
+package Strings;
+import java.util.*;
+public class LowertoUpperConvert {
+    public static void main(String[] agrs)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String t="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                t=t+(char)(ch-32);
+            }
+            else{
+                t=t+ch;
+            }
+        }
+        System.out.println(t);
+    }
+}
