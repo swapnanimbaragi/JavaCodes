843303ac7936db3ad3351f5625b43057939c2221
Swapna <swapnanimbaragi@gmail.com>
2026-03-16 17:08:58 +0530

    String

---
+++ Strings/MoveSpecialChar.java
@@ -0,0 +1,24 @@
+package Strings;
+import java.util.Scanner;
+
+public class MoveSpecialChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String letters = "";
+        String specialChars = "";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
+            {
+                letters=letters+ch;
+            }
+            else{
+                specialChars+=ch;
+            }
+
+        }
+        System.out.print(letters+specialChars);
+    }
+}
