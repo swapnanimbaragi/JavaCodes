62e3eeac9ba4ebb96dd9f89ca410f116ca602cc5
Swapna <swapnanimbaragi@gmail.com>
2026-03-16 18:43:07 +0530

    String

---
+++ Strings/RemoveNumeric.java
@@ -0,0 +1,24 @@
+package Strings;
+import java.util.*;
+public class RemoveNumeric {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String t="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='0' && ch<='9')
+            {
+                continue;
+            }
+            else
+            {
+                t=t+ch;
+            }
+                
+            
+        }
+        System.out.print(t);
+    }
+}
