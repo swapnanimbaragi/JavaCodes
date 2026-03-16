0c1343806a28b4a8882b9eb8f550b2ae1e8b635e
Swapna <swapnanimbaragi@gmail.com>
2026-03-16 18:53:21 +0530

    String

---
+++ Strings/RemoveAlphaNumeric.java
@@ -0,0 +1,24 @@
+package Strings;
+import java.util.*;
+public class RemoveAlphaNumeric {
+   public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String t="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
+            {
+                continue;
+            }
+            else
+            {
+                t=t+ch;
+            }
+                
+            
+        }
+        System.out.print(t);
+   } 
+}
