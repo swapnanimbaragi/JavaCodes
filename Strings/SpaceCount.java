c69850cae3b5fe839d8115f604f71f80b00ab2ca
Swapna <swapnanimbaragi@gmail.com>
2026-03-17 17:19:51 +0530

    String

--- Strings/SpaceCount.java
+++ Strings/SpaceCount.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SpaceCount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the String: ");
         String input = scanner.nextLine();
         int count=0;
         for(int i=0;i<input.length();i++)
