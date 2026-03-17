c6d1652ad9291f52b0c9a93932b7036acc8f4402
Swapna <swapnanimbaragi@gmail.com>
2026-03-17 17:09:24 +0530

    String

--- Strings/MoveSpecialChar.java
+++ Strings/MoveSpecialChar.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class MoveSpecialChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String"); 
         String s = sc.nextLine();
         String letters = "";
         String specialChars = "";
