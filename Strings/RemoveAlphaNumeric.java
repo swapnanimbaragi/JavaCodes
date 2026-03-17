4ea7c9b2c23c32f3b02348c983a8f207fbea16c4
Swapna <swapnanimbaragi@gmail.com>
2026-03-17 17:05:18 +0530

    String

--- Strings/RemoveAlphaNumeric.java
+++ Strings/RemoveAlphaNumeric.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class RemoveAlphaNumeric {
    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
         String s=sc.nextLine();
         String t="";
         for(int i=0;i<s.length();i++)
