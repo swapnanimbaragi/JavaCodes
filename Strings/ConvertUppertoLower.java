d88ed8099d238713341d07079631472f8561a910
Swapna <swapnanimbaragi@gmail.com>
2026-03-18 19:03:36 +0530

    String

--- Strings/ConvertUppertoLower.java
+++ Strings/ConvertUppertoLower.java
@@ -5,6 +5,7 @@ public class ConvertUppertoLower
     public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String: ");
         String s=sc.nextLine();
         String t="";
         for(int i=0;i<s.length();i++)
