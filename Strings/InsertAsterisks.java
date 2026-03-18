f7b2ac7c11f8dc9c6638b7e68aab7eaf8c678e6c
Swapna <swapnanimbaragi@gmail.com>
2026-03-18 19:01:44 +0530

    String

--- Strings/InsertAsterisks.java
+++ Strings/InsertAsterisks.java
@@ -4,6 +4,7 @@ public class InsertAsterisks {
     public static void main(String [] args)
     {
         Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the String: ");
         String s=scan.next();
         String t="";
         for(int i=0;i<s.length();i++)
