77fc251c715d365f2001966393264242589b3ea8
Swapna <swapnanimbaragi@gmail.com>
2026-03-20 22:21:43 +0530

    String

---
+++ Strings/RemoveDuplicateChar.java
@@ -0,0 +1,29 @@
+package Strings;
+import java.util.*;
+public class RemoveDuplicateChar {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        String s=scan.nextLine();
+        String t="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            int found=0;
+            for(int j=0; j<t.length();j++)
+            {
+                if(ch==t.charAt(j))
+                {
+                    found=1;
+                    break;
+                }
+            }
+            if(found==0)
+            {
+                t=t+ch;
+            }
+        }
+        System.out.print(t);
+    }
+}
+
