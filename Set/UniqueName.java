82a1c942bfd6311099f2f79abdb595784dcd2393
Swapna <swapnanimbaragi@gmail.com>
2026-03-25 22:29:03 +0530

    Set

---
+++ Set/UniqueName.java
@@ -0,0 +1,21 @@
+package Set;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+public class UniqueName {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter string");
+        String s=scan.nextLine();
+        LinkedHashSet<String> set=new LinkedHashSet<>();
+        String arr[]=s.split(" ");
+        for(String word : arr)
+        {
+            set.add(word);
+        }
+        for(String word : set)
+        {
+            System.out.print(word+" ");
+        }
+    }
+}
