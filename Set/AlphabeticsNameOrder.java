1eafc03b0ac56093360a4ea38e97465ede03fe43
Swapna <swapnanimbaragi@gmail.com>
2026-03-26 23:56:36 +0530

    Set

--- Set/AlphabeticsNameOrder.java
+++ Set/AlphabeticsNameOrder.java
@@ -8,6 +8,7 @@ public class AlphabeticsNameOrder
         System.out.println("enter number of names");
         int n=scan.nextInt();
         scan.nextLine();
+        System.out.println("enter names");
         String arr[]=new String[n];
         for(int i=0;i<arr.length;i++)
         {
