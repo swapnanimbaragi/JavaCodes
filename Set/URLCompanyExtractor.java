47d2245798d3bdd68abbdc30faaeecf56701f473
Swapna <swapnanimbaragi@gmail.com>
2026-03-27 15:17:12 +0530

    Set

--- Set/URLCompanyExtractor.java
+++ Set/URLCompanyExtractor.java
@@ -10,13 +10,9 @@ public class URLCompanyExtractor {
         for(int i=0;i<arr.length;i++)
         {
             String url = arr[i].trim();
-
-            // Remove "www."
             if (url.startsWith("www.")) {
                 url = url.substring(4);
             }
-
-            // Remove ".com"
             if (url.endsWith(".com")) {
                 url = url.substring(0, url.length() - 4);
             }
