51239acd4b22d735d4f96b7b68f32b202b0a0856
Swapna <swapnanimbaragi@gmail.com>
2026-03-27 14:57:53 +0530

    Set

--- Set/ConsonantsAlphabetics.java
+++ Set/ConsonantsAlphabetics.java
@@ -5,20 +5,25 @@ public class ConsonantsAlphabetics
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.println("enter string");
-        String s=scan.nextLine();
+        String s=scan.nextLine().toLowerCase();
         TreeSet<Character> set=new TreeSet<>();
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if( Character.isLetter(ch) && !(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') &&
-            !(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'))
+            if( Character.isLetter(ch) && !(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'))
+            
             {
                 set.add(Character.toLowerCase(ch));
             }
+        }
+        if(set.isEmpty())
+        {
+            System.out.println("no consonants found");
         }
         for(Character ch : set)
         {
             System.out.print(ch+" ");
         }
+        
     }
 }
