a19442da081896e2f40f388e46f0aa93a2b2043d
Swapna <swapnanimbaragi@gmail.com>
2026-03-27 15:16:22 +0530

    Set

---
+++ Set/URLCompanyExtractor.java
@@ -0,0 +1,30 @@
+package Set;
+import java.util.*;
+public class URLCompanyExtractor {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter url");
+        String s=scan.nextLine();
+        String arr[]=s.split(",");
+        TreeSet<String> set=new TreeSet<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            String url = arr[i].trim();
+
+            // Remove "www."
+            if (url.startsWith("www.")) {
+                url = url.substring(4);
+            }
+
+            // Remove ".com"
+            if (url.endsWith(".com")) {
+                url = url.substring(0, url.length() - 4);
+            }
+            set.add(url);
+        }
+        for(String word : set)
+        {
+            System.out.println(word+" ");
+        }
+    }
+}
