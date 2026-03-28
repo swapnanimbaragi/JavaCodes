a95549af7fc87ad73b65e0f0e067d4757e27099c
Swapna <swapnanimbaragi@gmail.com>
2026-03-28 22:03:30 +0530

    Map

---
+++ Map/FirstRepeatingElement.java
@@ -0,0 +1,28 @@
+package Map;
+import java.util.*;
+
+public class FirstRepeatingElement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (set.contains(arr[i])) {
+                System.out.println(arr[i]);
+                return; // stop after first repeating
+            } else {
+                set.add(arr[i]);
+            }
+        }
+
+        System.out.println("No repeating element");
+    }
+}
