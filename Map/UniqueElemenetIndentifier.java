96d1aca5b98b711049dc0185e6240abf22caf7c3
Swapna <swapnanimbaragi@gmail.com>
2026-03-29 19:46:09 +0530

    Map

---
+++ Map/UniqueElemenetIndentifier.java
@@ -0,0 +1,34 @@
+package Map;
+import java.util.*; 
+public class UniqueElemenetIndentifier {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        boolean found = false;
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                System.out.print(key + " ");
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println(-1);
+        }
+        
+    }
+}
