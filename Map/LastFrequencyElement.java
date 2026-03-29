d351f1088ea53f7bce9abcb586ee31fbdfa1ae58
Swapna <swapnanimbaragi@gmail.com>
2026-03-29 17:27:34 +0530

    Map

---
+++ Map/LastFrequencyElement.java
@@ -0,0 +1,34 @@
+package Map;    
+import java.util.*;
+class LastFrequencyElement
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if (map.get(arr[i]) > 1) 
+            {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+}   
