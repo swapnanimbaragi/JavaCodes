d01b71b16cd0d6b40a2cc59d8c9ff5e927fa9a58
Swapna <swapnanimbaragi@gmail.com>
2026-03-31 23:00:37 +0530

    Inheritance

--- Inheritence/AnimalSoundGenertore.java
+++ Inheritence/AnimalSoundGenertore.java
@@ -4,27 +4,22 @@ class Animal
 {
     private String name;
     private String age;
-
+    private String sound;
     public void setName(String name) {
         this.name = name;
     }
-
     public void setAge(String age) {
         this.age = age;
     }
-
     public String getName() {
         return name;
     }
-
     public String getAge() {
         return age;
     }
-
     public String getSound() {
         return sound;
     }
-
     public String getInfo()
     {
         return "";
