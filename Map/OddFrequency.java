4239c6805eede3eae15d4c62608bfc2b602ba0ba
Swapna <swapnanimbaragi@gmail.com>
2026-04-01 21:09:01 +0530

    Map

--- Map/OddFrequency.java
+++ Map/OddFrequency.java
@@ -5,6 +5,7 @@ public class OddFrequency {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
+        System.out.println("Enter the elements:");  
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
