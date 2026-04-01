906f5a627a29e77d733964d758ecbd7407c6fb5e
Swapna <swapnanimbaragi@gmail.com>
2026-04-01 21:06:02 +0530

    Map

---
+++ Map/OddFrequency.java
@@ -0,0 +1,35 @@
+package Map;
+import java.util.*;
+public class OddFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0;i<n;i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+                int count=map.get(arr[i]);
+                map.put(arr[i],count+1);
+            }
+            else
+            {
+                map.put(arr[i],1);
+            }
+        }
+        Set<Integer> keys=map.keySet();
+        for(Integer key:keys)
+        {
+            if(map.get(key)%2!=0)
+            {
+                System.out.println(key);
+            }
+        }
+
+    }
+}
