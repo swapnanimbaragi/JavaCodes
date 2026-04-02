3bc8b2f08b7e0839dc1daff464ba06d5d01a2b95
Swapna <swapnanimbaragi@gmail.com>
2026-04-02 16:22:26 +0530

    MultiplesTillM

---
+++ Practice/MultipleTillN.java
@@ -0,0 +1,16 @@
+package Practice;
+import java.util.*;
+public class MultipleTillN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for(int i=n; i<=m; i++)
+        {
+            if(i%n==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
