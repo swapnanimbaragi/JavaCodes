eb77b5d8ecfe7d215ba8bc321b38d0e06d36f15e
Swapna <swapnanimbaragi@gmail.com>
2026-04-02 16:04:56 +0530

    Mulitiples

---
+++ Practice/FirstNMultiplesOf3Desending.java
@@ -0,0 +1,13 @@
+package Practice;
+import java.util.*;
+public class FirstNMultiplesOf3Desending {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n; i>=1; i--)
+        {
+            System.out.print(3*i+" ");
+        }
+    }
+}
