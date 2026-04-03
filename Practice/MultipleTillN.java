7bb9b340dff5e82382edbe2383a6c3fafb4c74ff
Swapna <swapnanimbaragi@gmail.com>
2026-04-03 21:26:23 +0530

    Practice

--- Practice/MultipleTillN.java
+++ Practice/MultipleTillN.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class MultipleTillN {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers");
         int n=sc.nextInt();
         int m=sc.nextInt();
         for(int i=n; i<=m; i++)
