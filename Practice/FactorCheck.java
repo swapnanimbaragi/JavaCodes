047c44de383156277d9feab2b465e2f063cc5c13
Swapna <swapnanimbaragi@gmail.com>
2026-04-03 21:22:21 +0530

    Practice

--- Practice/FactorCheck.java
+++ Practice/FactorCheck.java
@@ -4,6 +4,7 @@ public class FactorCheck {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
         int M=sc.nextInt();
         int N=sc.nextInt();
         if(M%N==0)
