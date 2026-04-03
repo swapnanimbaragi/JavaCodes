7ff2d6aa8669d4927deabe2d4a579824d370bff8
Swapna <swapnanimbaragi@gmail.com>
2026-04-03 21:21:05 +0530

    Practice

--- Practice/MultiplesOf3BetweenN1andN2.java
+++ Practice/MultiplesOf3BetweenN1andN2.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class MultiplesOf3BetweenN1andN2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
         int n1=sc.nextInt();
         int n2=sc.nextInt();
         for(int i=n1; i<=n2; i++)
