a93f19c91605f2d73c34e45d04e24de07fec77bf
Swapna <swapnanimbaragi@gmail.com>
2026-04-03 21:17:52 +0530

    First

--- Practice/FirstNMultiplesOf3Desending.java
+++ Practice/FirstNMultiplesOf3Desending.java
@@ -4,6 +4,7 @@ public class FirstNMultiplesOf3Desending {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
         int n=sc.nextInt();
         for(int i=n; i>=1; i--)
         {
