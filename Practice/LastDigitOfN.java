94eb81121747ed82ec856ae2c53680acbc42889b
Swapna <swapnanimbaragi@gmail.com>
2026-04-03 21:20:16 +0530

    Practice

--- Practice/LastDigitOfN.java
+++ Practice/LastDigitOfN.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class LastDigitOfN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
         int n=sc.nextInt();
         int lastDigit=n%10;
         System.out.println(lastDigit);
