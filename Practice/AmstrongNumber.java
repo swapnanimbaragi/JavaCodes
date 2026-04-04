ab9dee712fb9c9926f36d8e77e8442b55cbb37ed
Swapna <swapnanimbaragi@gmail.com>
2026-04-04 23:22:33 +0530

    Map

---
+++ Practice/AmstrongNumber.java
@@ -0,0 +1,10 @@
+package Practice;
+import java.util.*;
+public class AmstrongNumber {
+    
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+
+    }
+}
