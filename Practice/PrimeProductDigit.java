76def8c922ddf133100ef6a0e5f91ac9c6209ea6
Swapna <swapnanimbaragi@gmail.com>
2026-04-10 15:28:33 +0530

    Practice

---
+++ Practice/PrimeProductDigit.java
@@ -0,0 +1,43 @@
+package Practice;
+import java.util.*;
+public class PrimeProductDigit 
+{
+    public static boolean isPrime(int n)
+    {
+        int count=0;
+        for(int i=2;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                count++;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int product=1;
+        while(n>0)
+        {
+            int last= n%10;
+            if(isPrime(last))
+            {
+                product= product*last;
+            }
+            n=n/10;
+        }
+        if(isPrime(product))
+        {
+            System.out.print("Prime");
+        }
+        else
+        {
+            System.out.print("Not Prime");
+        }
+    }
+}
