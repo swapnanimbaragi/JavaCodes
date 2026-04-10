6e3a6b91c91d3a04f4fc7189cdce439931d7934e
Swapna <swapnanimbaragi@gmail.com>
2026-04-10 14:29:13 +0530

    Practice

--- Practice/AmstrongNumber.java
+++ Practice/AmstrongNumber.java
@@ -5,6 +5,18 @@ public class AmstrongNumber {
     public static void main(String[] args)
     {
         Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        int count = String.valueOf(num).length();
+        int arm = 0;
+        while (num > 0) 
+        {
+            int last= num % 10;
+            arm = arm + (int)Math.pow(last, count);
+            num = num / 10;
+        }
+        System.out.println("The Armstrong number is: "+arm);
+
 
     }
 }
