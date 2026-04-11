383a0efde22b7d7db30232da8638753c5ac1cc9c
Swapna <swapnanimbaragi@gmail.com>
2026-04-11 09:00:58 +0530

    Practice

---
+++ Practice/MatchingSumSubarray.java
@@ -0,0 +1,46 @@
+package Practice;
+import java.util.*;
+public class MatchingSumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k=sc.nextInt();
+        int m=sc.nextInt();
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        
+
+        for(int size=1;size<=n;size++)
+        {
+            for(int i=0;i<=n-size;i++)
+            {
+                int sum = 0;
+
+                // calculate full subarray sum
+                for(int j=i;j<i+size;j++)
+                {
+                    sum += arr[j];   
+                }
+
+                // check even sum
+                if(sum == m )
+                {
+                    // print subarray
+                    for(int j=i;j<i+size;j++)
+                    {
+                        System.out.print(arr[j] + " ");
+                    }
+                    // System.out.println();
+                    return;
+                }
+            }
+
+        }
+    }
+}
