7702b29ef0be3fef7d3c53785231e12dbd584030
Swapna <swapnanimbaragi@gmail.com>
2026-04-11 08:21:36 +0530

    Practice

---
+++ Practice/EvenSubArray.java
@@ -0,0 +1,41 @@
+package Practice;
+import java.util.*;
+
+public class EvenSubArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int size=1;size<=n;size++)
+        {
+            for(int i=0;i<=n-size;i++)
+            {
+                int sum = 0;
+
+                // calculate full subarray sum
+                for(int j=i;j<i+size;j++)
+                {
+                    sum += arr[j];   // ✅ FIX HERE
+                }
+
+                // check even sum
+                if(sum % 2 == 0)
+                {
+                    // print subarray
+                    for(int j=i;j<i+size;j++)
+                    {
+                        System.out.print(arr[j] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
