64b2bd4a4535cf71057ffb016c6f2f651136ee71
Swapna <swapnanimbaragi@gmail.com>
2026-04-11 21:41:25 +0530

    Practice

---
+++ Practice/CharOccurance.java
@@ -0,0 +1,37 @@
+package Practice;
+import java.util.*;
+public class CharOccurance {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        LinkedHashMap<Character,Integer> map=new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch== ' ')
+            {
+                continue;
+            }
+            
+            if(map.containsKey(ch))
+            {
+                int count=map.get(ch);
+                map.put(ch,count+1);
+            }
+            else
+            {
+                map.put(ch,1);
+            }
+            
+        }
+        Set<Character> keys=map.keySet();
+        for(Character key:keys)
+        {
+            if(map.get(key)%2==0)
+            {
+                System.out.println(key+" "+map.get(key));
+            }
+        }
+    }
+}
