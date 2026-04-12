837c4385ff0623cadcb2e0e6a26dde5184eb4312
Swapna <swapnanimbaragi@gmail.com>
2026-04-12 15:55:05 +0530

    Practice

--- Practice/PrimeProductSubarray.java
+++ Practice/PrimeProductSubarray.java
@@ -6,9 +6,10 @@ public class PrimeProductSubarray {
     public static boolean isPrime(int n)
     {
         if(n <= 1) 
+        {
             return false;
-
-        for(int i = 2; i <= Math.sqrt(n); i++)
+        }
+        for(int i = 2; i <= n/2; i++)
         {
             if(n % i == 0)
                 return false;
