e73018389391c3516451950ccd423854b5b4e361
Swapna <swapnanimbaragi@gmail.com>
2026-04-12 09:59:12 +0530

    Practice

--- Practice/MargeTwoArrayExam.java
+++ Practice/MargeTwoArrayExam.java
@@ -1,6 +1,6 @@
 package Practice;
 import java.util.*;
-public class MargeTwoArray {
+public class MargeTwoArrayExam {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
