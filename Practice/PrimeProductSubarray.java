780587b31598953998f5dca603664ed7d75a00c2
Swapna <swapnanimbaragi@gmail.com>
2026-04-12 15:55:40 +0530

    Practice

--- Practice/PrimeProductSubarray.java
+++ Practice/PrimeProductSubarray.java
@@ -12,7 +12,9 @@ public class PrimeProductSubarray {
         for(int i = 2; i <= n/2; i++)
         {
             if(n % i == 0)
+            {
                 return false;
+            }
         }
         return true;
     }
