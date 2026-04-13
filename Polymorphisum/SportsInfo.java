948afd10215f2181db9a9e13591a4ea820c4b052
Swapna <swapnanimbaragi@gmail.com>
2026-04-13 22:18:26 +0530

    Polymorphisum

---
+++ Polymorphisum/SportsInfo.java
@@ -0,0 +1,56 @@
+package Polymorphisum;
+import java.util.Scanner;   
+class Sports
+{
+    void info()
+    {
+        System.out.println("Sports is a physical activity");
+    }
+}
+class Football extends Sports
+{
+    void info()
+    {
+        System.out.println("Football is a team sport");
+    }
+}
+class Basketball extends Sports
+{
+    void info()
+    {
+        System.out.println("Basketball is a team sport");
+    }
+}
+class Rubby extends Sports
+{
+    void info()
+    {
+        System.out.println("Rubby is a team sport");
+    }
+}
+public class SportsInfo {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        Sports sp;
+        if(str.equals("Football"))
+        {
+            sp=new Football();
+            sp.info();
+        }
+        else if(str.equals("Basketball"))
+        {
+            sp=new Basketball();
+            sp.info();
+        }
+        else if(str.equals("Rubby"))
+        {
+            sp=new Rubby();
+            sp.info();
+        }
+        // else
+        // {
+        //     System.out.println("Unknown sport");
+        // }
+    }
+}
