34733288595edebf21c1e8f2c66bd7e0b3c08e7e
Swapna <swapnanimbaragi@gmail.com>
2026-04-13 22:56:06 +0530

    Polymorphisum

---
+++ Polymorphisum/EmployeeSalary.java
@@ -0,0 +1,46 @@
+package Polymorphisum;
+import java.util.Scanner;
+class Employee
+{
+    void salary()
+    {
+        System.out.println("Employee has a salary");
+    }
+}
+class Manager extends Employee
+{
+    void salary()
+    {
+        System.out.println("Manager has a salary of 50000");
+    }
+}
+class Developer extends Employee
+{
+    void salary()
+    {
+        System.out.println("Developer has a salary of 40000");
+    }
+}
+public class EmployeeSalary 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        Employee e;
+        if(str.equals("Manager"))
+        {
+            e=new Manager();
+            e.salary();
+        }
+        else if(str.equals("Developer"))
+        {
+            e=new Developer();
+            e.salary();
+        }
+        else
+        {
+            System.out.println("Unknown employee");
+        }
+}
+}
