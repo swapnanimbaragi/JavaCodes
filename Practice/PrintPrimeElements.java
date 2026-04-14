ca39e69d6cf8a2a67217e50b57afeba856db4e49
Swapna <swapnanimbaragi@gmail.com>
2026-04-14 16:20:09 +0530

    Practice

--- Practice/PrintPrimeElements.java
+++ Practice/PrintPrimeElements.java
@@ -1,24 +1,7 @@
 package Practice;
 import java.util.*;
-public class PrintPrimeElements {
-    public static void main(String[] args) 
-    {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=s.nextInt();
-        }
-        System.out.println("Prime elements in the array:");
-        for(int i=0;i<n;i++)
-        {
-            if(isPrime(arr[i]))
-            {
-                System.out.print(arr[i]+" ");
-            }
-        }
-    }
+public class PrintPrimeElements 
+{
     public static boolean isPrime(int n)
     {
         if(n<=1)
@@ -33,5 +16,30 @@ public class PrintPrimeElements {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        boolean primeFound=false;
+        System.out.println("Prime elements in the array:");
+        for(int i=0;i<n;i++)
+        {
+            if(isPrime(arr[i]))
+            {
+                System.out.print(arr[i]+" ");
+                primeFound=true;
+            }
+        }
+        if(!primeFound)
+        {
+            System.out.println("No prime elements found in the array.");
+        }
     }
 }
