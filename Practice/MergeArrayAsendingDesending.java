5e4f5267134a404b82d1c53eb496db1506ab24ce
Swapna <swapnanimbaragi@gmail.com>
2026-04-17 14:21:06 +0530

    Practice

--- Practice/MergeArrayAsendingDesending.java
+++ Practice/MergeArrayAsendingDesending.java
@@ -1,33 +1,54 @@
 package Practice;
 import java.util.*;
+
 public class MergeArrayAsendingDesending {
-    Public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+
         int n1 = sc.nextInt();
         int[] arr1 = new int[n1];
         for(int i = 0; i < n1; i++)
         {
             arr1[i] = sc.nextInt();
         }
+
         int n2 = sc.nextInt();
         int[] arr2 = new int[n2];
         for(int i = 0; i < n2; i++)
         {
             arr2[i] = sc.nextInt();
         }
+
         int[] mergedArr = new int[n1 + n2];
-        for(int i=0;i<n1;i++)
+
+        int i = 0, j = n2 - 1, k = 0;   // ✅ j from end
+
+        while(i < n1 && j >= 0)
         {
-            mergedArr[i] = arr1[i];
+            if(arr1[i] < arr2[j])
+            {
+                mergedArr[k++] = arr1[i++];
+            }
+            else
+            {
+                mergedArr[k++] = arr2[j--];
+            }
         }
-        for(int i=0;i<n2;i++)
+
+        while(i < n1)
         {
-            mergedArr[n1 + i] = arr2[i];
+            mergedArr[k++] = arr1[i++];
         }
-        Arrays.sort(mergedArr);
-        for(int i=0;i<mergedArr.length;i++)
+
+        while(j >= 0)
         {
-            System.out.print(mergedArr[i] + " ");
+            mergedArr[k++] = arr2[j--];
+        }
+
+        for(int x = 0; x < mergedArr.length; x++)
+        {
+            System.out.print(mergedArr[x] + " ");
         }
     }
 }
