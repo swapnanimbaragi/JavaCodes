1f1ff98660e0017b574a30d7f6d92fb45fe32686
Swapna <swapnanimbaragi@gmail.com>
2026-04-17 15:08:03 +0530

    ExamQuestion

--- ExamQuestions/RotateArrayByK.java
+++ ExamQuestions/RotateArrayByK.java
@@ -1,15 +1,15 @@
-package Practice;
-import java.util.*;
-public class RotateArrayByKsteps {
+package ExamQuestions;
+import java.util.Scanner;
+public class RotateArrayByK {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k=sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
         {
             arr[i] = sc.nextInt();
         }
+        int k= sc.nextInt();
         int res[]=new int[n];
         for(int i=0;i<n;i++)
         {
@@ -21,4 +21,3 @@ public class RotateArrayByKsteps {
         }
     }
 }
-
