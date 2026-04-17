898b441560ff5d771219ab0d00c783cdc0c2d753
Swapna <swapnanimbaragi@gmail.com>
2026-04-17 13:55:10 +0530

    Practice

---
+++ Practice/MergeSortedArray.java
@@ -0,0 +1,33 @@
+package Practice;
+import java.util.*;
+class MergeSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        for(int i = 0; i < n1; i++)
+        {
+            arr1[i] = sc.nextInt();
+        }
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        for(int i = 0; i < n2; i++)
+        {
+            arr2[i] = sc.nextInt();
+        }
+        int[] mergedArr = new int[n1 + n2];
+        for(int i=0;i<n1;i++)
+        {
+            mergedArr[i] = arr1[i];
+        }
+        for(int i=0;i<n2;i++)
+        {
+            mergedArr[n1 + i] = arr2[i];
+        }
+        Arrays.sort(mergedArr);
+        for(int i=0;i<mergedArr.length;i++)
+        {
+            System.out.print(mergedArr[i] + " ");
+        }
+    }
+}
