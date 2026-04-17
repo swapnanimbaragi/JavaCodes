519dfc0fab35949cf9cd451204090cc7c7740c19
Swapna <swapnanimbaragi@gmail.com>
2026-04-17 14:27:14 +0530

    Practice

--- Practice/MergeArrayAsendingDesending.java
+++ Practice/MergeArrayAsendingDesending.java
@@ -19,33 +19,17 @@ public class MergeArrayAsendingDesending {
         {
             arr2[i] = sc.nextInt();
         }
-
         int[] mergedArr = new int[n1 + n2];
-
-        int i = 0, j = n2 - 1, k = 0;   // ✅ j from end
-
-        while(i < n1 && j >= 0)
+         // ✅ Sort the merged array
+        for(int i=0;i<n1;i++)
         {
-            if(arr1[i] < arr2[j])
-            {
-                mergedArr[k++] = arr1[i++];
-            }
-            else
-            {
-                mergedArr[k++] = arr2[j--];
-            }
+            mergedArr[i] = arr1[i];
         }
-
-        while(i < n1)
+        for(int i=0;i<n2;i++)
         {
-            mergedArr[k++] = arr1[i++];
+            mergedArr[n1 + i] = arr2[i];
         }
-
-        while(j >= 0)
-        {
-            mergedArr[k++] = arr2[j--];
-        }
-
+        Arrays.sort(mergedArr); 
         for(int x = 0; x < mergedArr.length; x++)
         {
             System.out.print(mergedArr[x] + " ");
