5925458cf4c0e1a6edcd53327aab88d38f113266
Swapna <swapnanimbaragi@gmail.com>
2026-04-18 22:52:47 +0530

    Practice

---
+++ Practice/SumOfFirstandLast.java
@@ -0,0 +1,16 @@
+package Practice;
+import java.util.*;
+public class SumOfFirstandLast {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int firstDigit=n;
+        while(firstDigit>=10)
+        {
+            firstDigit/=10;
+        }
+        int lastDigit=n%10;
+        int sum=firstDigit+lastDigit;
+        System.out.println("Sum of first and last digit:"+sum);
+    }
+}
