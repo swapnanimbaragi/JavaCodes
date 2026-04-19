ab4daf202e72328b97f6bfa18fc4fcf771e1c489
Swapna <swapnanimbaragi@gmail.com>
2026-04-19 22:07:14 +0530

    Exam

--- ExamQuestions/RotateArrayByK.java
+++ ExamQuestions/RotateArrayByK.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class RotateArrayByK {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = scanner.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
         {
-            arr[i] = sc.nextInt();
+            arr[i] = scanner.nextInt();
         }
-        int k= sc.nextInt();
+        int k= scanner.nextInt();
         int res[]=new int[n];
         for(int i=0;i<n;i++)
         {
