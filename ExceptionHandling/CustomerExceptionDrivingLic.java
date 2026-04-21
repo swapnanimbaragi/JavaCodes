ac8870042e1748e54c851ccafa3681924805a458
Swapna <swapnanimbaragi@gmail.com>
2026-04-21 20:53:16 +0530

    Exception

--- ExceptionHandling/CustomerExceptionDrivingLic.java
+++ ExceptionHandling/CustomerExceptionDrivingLic.java
@@ -8,32 +8,42 @@ class UnderAgeException extends Exception
     }
 }
 // Custom Exception for age > 60
-class OverAgeException extends Exception {
-    public OverAgeException(String msg) {
+class OverAgeException extends Exception 
+{
+    public OverAgeException(String msg) 
+    {
         super(msg);
     }
 }
 
-public class CustomerExceptionDrivingLic {
-    public static void main(String[] args) {
+public class CustomerExceptionDrivingLic 
+{
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
 
-        try {
-            if (age < 18) {
+        try 
+        {
+            if (age < 18) 
+            {
                 throw new UnderAgeException("You are too young. Have patience!");
             } 
-            else if (age > 60) {
+            else if (age > 60) 
+            {
                 throw new OverAgeException("You are too old. Cool down!");
             } 
-            else {
+            else 
+            {
                 System.out.println("Eligibility confirmed: You can apply for a driving license.");
             }
         } 
-        catch (UnderAgeException e) {
+        catch (UnderAgeException e) 
+        {
             System.out.println(e.getMessage());
         } 
-        catch (OverAgeException e) {
+        catch (OverAgeException e) 
+        {
             System.out.println(e.getMessage());
         }
     }
