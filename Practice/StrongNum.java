f449b7e7b9c32447cc1197b46cca8c1c818fe6d2
Swapna <swapnanimbaragi@gmail.com>
2026-04-22 18:22:43 +0530

    Practice

---
+++ Practice/StrongNum.java
@@ -0,0 +1,37 @@
+package Practice;
+import java.util.*;
+public class  StrongNum {
+    public static int isFactorial (int n)
+    {
+        int fact = 1;
+        for(int i = 1; i <= n; i++)   // ✔ fixed
+        {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();   // input
+
+        for(int num = 1; num <= n; num++)   // ✔ loop till n
+        {
+            int temp = num;
+            int sum = 0;
+
+            while(temp > 0)
+            {
+                int digit = temp % 10;
+                sum += isFactorial(digit);
+                temp /= 10;
+            }
+
+            if(sum == num)
+            {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
+
--- Practice/StrongNumbers.java
+++ Practice/StrongNumbers.java
@@ -1,7 +1,7 @@
 package Practice;
 import java.util.*;
-public class StronNumber {
-    public static int isFactorial(int n)
+public class StrongNumbers {
+    public static int isFactorial (int n)
     {
         int fact=1;
         for(int i=1;i<=n;i++)
@@ -9,25 +9,15 @@ public class StronNumber {
             fact*=i;
         }
         return fact;
-    } 
+    }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
         int original=n;
-        int sum=0;
         while(n>0)
         {
             int digit=n%10;
-            sum+=isFactorial(digit);
-            n=n/10;
+
         }
-        if(sum==original)
-        {
-            System.out.print("Strong Number");
-        }
-        else{
-            System.out.print("Not Strong Number");
-        }
-        
     }
 }
