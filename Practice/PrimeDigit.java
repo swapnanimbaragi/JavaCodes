3794fbe7af3072eb8f180975de38c1e8f01348ca
Swapna <swapnanimbaragi@gmail.com>
2026-04-22 16:35:40 +0530

    Practice

---
+++ Practice/PrimeDigit.java
@@ -0,0 +1,39 @@
+package Practice;
+import java.util.*;
+
+public class PrimeDigit {
+
+    public static boolean isPrime(int num) {
+        if(num <= 1) {
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        boolean hasPrimeDigit = false;
+
+        while(n > 0) {
+            int digit = n % 10;
+
+            if(isPrime(digit)) {   // ✔ FIXED
+                System.out.print(digit + " ");
+                hasPrimeDigit = true;
+            }
+
+            n /= 10;
+        }
+
+        if(!hasPrimeDigit) {
+            System.out.println("Does Not Contain Prime Digit");
+        }
+    }
+}
