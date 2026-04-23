4efd59f980f6d8f80535b7717e4d29c124f26d38
Swapna <swapnanimbaragi@gmail.com>
2026-04-23 21:51:28 +0530

    Practice

--- Practice/BinarToDecimal.java
+++ Practice/BinarToDecimal.java
@@ -1,16 +1,17 @@
 package Practice;
 import java.util.*;
 public class BinarToDecimal {
-    public static void main(String[] args) {
-    Scanner scan=new Scanner(System.in);
-    int n=scan.nextInt();
-    String binary="";
-    while(n>0)
+    public static void main(String[] args) 
     {
-        int rem=n%2;
-        binary=rem+binary;
-        n=n/2;
-    }
-    System.out.print("Binary To Decimal"+binary);
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        String binary="";
+        while(n>0)
+        {
+            int rem=n%2;
+            binary=rem+binary;
+            n=n/2;
+        }
+        System.out.print("Binary To Decimal"+binary);
     }
 }
