29df9234d98246b0c2c7dfcecec8c8303bf2c90c
Swapna <swapnanimbaragi@gmail.com>
2026-04-26 13:18:32 +0530

    Practice

---
+++ Practice/PairSumGraterThanK.java
@@ -0,0 +1,24 @@
+package Practice;
+import java.util.*;
+public class PairSumGraterThanK {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int k=scanner.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]+a[j]>k)
+                {
+                    System.out.println(a[i]+" "+a[j]);
+                }
+            }
+        }
+    }
+}
