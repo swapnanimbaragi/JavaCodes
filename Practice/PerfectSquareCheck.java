cc84e8c3f6c55fbdc39e9c44921fce50dd55b6f0
Swapna <swapnanimbaragi@gmail.com>
2026-04-27 21:14:09 +0530

    Practice

--- Practice/PerfectSquareCheck.java
+++ Practice/PerfectSquareCheck.java
@@ -4,7 +4,7 @@ public class PerfectSquareCheck {
     public static void main(String[] args)
     {
         Scanner scanner=new Scanner(System.in);
-        int n=scan.nextInt();
+        int n=scanner.nextInt();
         int root=(int)Math.sqrt(n);
         if(root*root==n)
         {
