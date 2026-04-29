6150011b966ec3dfd3fcbb9bbca0c6fafd800c02
Swapna <swapnanimbaragi@gmail.com>
2026-04-29 23:28:44 +0530

    Practice

--- Practice/LargestCollection.java
+++ Practice/LargestCollection.java
@@ -2,22 +2,22 @@ package Practice;
 import java.util.*;
 public class LargestCollection {
     public static void main(String[] args) {    
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int k=sc.nextInt();
-    int[] arr = new int[n];
-    for(int i=0;i<n;i++)
-    {
-        arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            list.add(arr[i]);
+        }
+        Collections.sort(list);
+        int largest = list.get(list.size()-k);
+        System.out.print("Kth largest Element:"+largest);
     }
-    ArrayList<Integer> list = new ArrayList<>();
-    for(int i=0;i<n;i++)
-    {
-        list.add(arr[i]);
-    }
-    Collections.sort(list);
-    int largest = list.get(list.size()-k);
-    System.out.print("Kth largest Element:"+largest);
-}
 }
 
