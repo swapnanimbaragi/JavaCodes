19448ddfd3025c12c81e184dc412f8430c067786
Swapna <swapnanimbaragi@gmail.com>
2026-04-29 23:29:11 +0530

    Practice

--- Practice/LargestCollection.java
+++ Practice/LargestCollection.java
@@ -1,7 +1,9 @@
 package Practice;
 import java.util.*;
-public class LargestCollection {
-    public static void main(String[] args) {    
+public class LargestCollection 
+{
+    public static void main(String[] args) 
+    {    
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k=sc.nextInt();
