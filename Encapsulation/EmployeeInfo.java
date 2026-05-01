5f4f63a04c77a58c18aa07780dea6fae965416b0
Swapna <swapnanimbaragi@gmail.com>
2026-05-01 20:38:08 +0530

    Encapsulation

--- Encapsulation/EmployeeInfo.java
+++ Encapsulation/EmployeeInfo.java
@@ -37,13 +37,16 @@ public class EmployeeInfo
     public String getDepartment() {
         return department;
     }
-    public void setDepartment(String department) {
+    public void setDepartment(String department) 
+    {
         this.department = department;
     }
-    public String getEmail() {
+    public String getEmail() 
+    {
         return email;
     }
-    public void setEmail(String email) {
+    public void setEmail(String email) 
+    {
         this.email = email;
     }
     public String getPhoneNumber() {
