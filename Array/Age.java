1f3494c7f03308d6c5ad3bffb42e900f978a3ddc
Swapna <swapnanimbaragi@gmail.com>
2026-05-06 23:07:44 +0530

    Array

--- Array/Age.java
+++ Array/Age.java
@@ -6,7 +6,7 @@ public class Age
 {
     public static void main(String[] args)
     {
-        Scanner scan=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         int a[]=new int[5];
         for(int i=0;i<5;i++)
         {
