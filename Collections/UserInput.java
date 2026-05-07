e9ffca94ec30f4128d2e0fc40347a4c13e3d6048
Swapna <swapnanimbaragi@gmail.com>
2026-05-07 21:33:09 +0530

    Collections

--- Collections/UserInput.java
+++ Collections/UserInput.java
@@ -5,10 +5,12 @@ public class UserInput {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         ArrayList list=new ArrayList();
-        for(int i=0;i<5;i++)
+        int n=scanner.nextInt();
+        for(int i=0;i<n;i++)
         {
             int num=scanner.nextInt();
             list.add(num);
         }
+        System.out.println(list);
     }
 }
