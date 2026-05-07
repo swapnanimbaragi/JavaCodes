a9b2b5a41baf391428e7c4e5c64c74a98c179bca
Swapna <swapnanimbaragi@gmail.com>
2026-05-07 21:43:59 +0530

    Collections

---
+++ Collections/OddNum.java
@@ -0,0 +1,20 @@
+package Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class OddNum {
+    public static void main(String[] args) 
+    {
+        Scanner scanner=new Scanner(System.in);
+        ArrayList list=new ArrayList();
+        int n=scanner.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int num=scanner.nextInt();
+            if(num%2!=0)
+            {
+                list.add(num);
+            }
+        }
+        System.out.println(list);
+    }
+}
