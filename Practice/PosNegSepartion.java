d49cc851e9cb1ed02e9bcca8060a8fd2f0edaab2
Swapna <swapnanimbaragi@gmail.com>
2026-05-08 17:31:06 +0530

    Practice

---
+++ Practice/PosNegSepartion.java
@@ -0,0 +1,30 @@
+package Practice;
+import java.util.Scanner;
+public class PosNegSepartion {
+    public static void main(String[] args) 
+    {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scanner.nextInt();
+        }
+        System.out.println("Positive numbers:");
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        System.out.println("\nNegative numbers:");
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
