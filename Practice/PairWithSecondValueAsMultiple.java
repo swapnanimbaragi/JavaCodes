ea590456313229b8c3c785520ac329b69840e749
Swapna <swapnanimbaragi@gmail.com>
2026-05-08 18:32:15 +0530

    Practice

---
+++ Practice/PairWithSecondValueAsMultiple.java
@@ -0,0 +1,31 @@
+package Practice;
+import java.util.Scanner;
+public class PairWithSecondValueAsMultiple {
+    public static void main(String[] args) 
+    {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scanner.nextInt();
+        }
+        boolean foundPair=false;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[j]%arr[i]==0)
+                {
+                    System.out.println("Pair: ("+arr[i]+", "+arr[j]+")");
+                    foundPair=true;
+                }
+            }
+        }
+        if(!foundPair)
+        {
+            System.out.println("No such pairs found.");
+        }
+    }
+}
