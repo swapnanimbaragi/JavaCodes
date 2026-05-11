0d27d7229c6de731f43f74b2aec2243682eb29a0
swapnanimbaragi <swapnanimbaragi@gmail.com>
2026-05-11 12:32:38 +0530

    Fix formatting issues in ArrayPairs class

--- Array/ArrayPairs.java
+++ Array/ArrayPairs.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class ArrayPairs {
+public class ArrayPairs 
+{
      public static void main(String[] args) 
      {
         Scanner scanner = new Scanner(System.in);
