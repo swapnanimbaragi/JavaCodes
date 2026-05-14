0a5a9894bea8f66f11842436333724847f60a315
Swapna <swapnanimbaragi@gmail.com>
2026-05-14 23:37:14 +0530

    Practice

---
+++ ExamQuestions/PrimeBtwnN1andN2.java
@@ -0,0 +1,27 @@
+package ExamQuestions;
+import java.util.*;
+public class PrimeBtwnN1andN2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        for(int i=n1; i<=n2; i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static boolean isPrime(int n) {
+        if(n<=1) {
+            return false;
+        }
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(n%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
