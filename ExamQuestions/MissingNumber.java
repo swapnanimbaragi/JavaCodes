92d51630169e78c5ea712407e551cb7fd7eb47c5
Swapna <swapnanimbaragi@gmail.com>
2026-05-14 12:39:04 +0530

    Practice

--- ExamQuestions/MissingNumber.java
+++ ExamQuestions/MissingNumber.java
@@ -1,30 +1,39 @@
 package ExamQuestions;
 import java.util.*;
-public class MissingNumber {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] arr = new int[n-1];
-        for(int i=0; i<n-1; i++)
-        {
-            arr[i] = scan.nextInt();
-        }
-        // int sum = n*(n+1)/2;
-        // for(int i=0; i<n-1; i++)
-        // {
-        //     sum -= arr[i];
-        // }
-        // System.out.println(sum);
-        int total=0;
-        for(int i=1; i<=n; i++)
+public class MissingNumber
+{
+    static int missingNum(int n, int arr[])
+    {
+        int total = 0;
+
+        // Adding numbers from 1 to n+1
+        for(int i=1; i<=n+1; i++)
         {
             total += i;
         }
-        // System.out.println(total);
-        for(int i=0; i< arr.length; i++)
+
+        // Subtracting array elements
+        for(int i=0; i<arr.length; i++)
         {
             total -= arr[i];
         }
-        System.out.println(total);
+
+        return total;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println(missingNum(n, arr));
     }
 }
