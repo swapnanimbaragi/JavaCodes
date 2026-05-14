e10234c503b58fecbf1607ab45c9c41f8ccc5bfc
Swapna <swapnanimbaragi@gmail.com>
2026-05-14 11:54:47 +0530

    Practice

--- ExamQuestions/MissingNumber.java
+++ ExamQuestions/MissingNumber.java
@@ -9,11 +9,22 @@ public class MissingNumber {
         {
             arr[i] = scan.nextInt();
         }
-        int sum = n*(n+1)/2;
-        for(int i=0; i<n-1; i++)
+        // int sum = n*(n+1)/2;
+        // for(int i=0; i<n-1; i++)
+        // {
+        //     sum -= arr[i];
+        // }
+        // System.out.println(sum);
+        int total=0;
+        for(int i=1; i<=n; i++)
         {
-            sum -= arr[i];
+            total += i;
         }
-        System.out.println(sum);
+        
+        for(int i=0; i< arr.length; i++)
+        {
+            total -= arr[i];
+        }
+        System.out.println(total);
     }
 }
