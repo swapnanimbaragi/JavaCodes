8f608f13b6eb17b72d1e2c22ed4f12d127bf067e
Swapna <swapnanimbaragi@gmail.com>
2026-05-16 21:54:04 +0530

    Practice

--- ExamQuestions/SumOfSubset.java
+++ ExamQuestions/SumOfSubset.java
@@ -9,7 +9,7 @@ public class SumOfSubset
         int k = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++) 
-            {
+        {
             arr[i] = sc.nextInt();
         }
         for(int i=0 ;i<n;i++)
