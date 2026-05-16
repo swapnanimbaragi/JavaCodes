a6b3b8e10b7037d6554b1fc9644e961294be354e
Swapna <swapnanimbaragi@gmail.com>
2026-05-16 21:52:44 +0530

    Practice

---
+++ ExamQuestions/SumOfSubset.java
@@ -0,0 +1,25 @@
+package ExamQuestions;
+import java.util.*;
+public class SumOfSubset {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0 ;i<n;i++)
+        {
+            for(int j=i+1 ;j<n;j++)
+            {
+                if(arr[i]+arr[j] == k)
+                {
+                    System.out.println(arr[i] + " " + arr[j]);
+                    
+                }
+            }
+        }
+           
+    }
+}
