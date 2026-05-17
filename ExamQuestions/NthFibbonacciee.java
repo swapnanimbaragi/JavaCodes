477a73f16debca7631252dbadc128c0af0b6a676
Swapna <swapnanimbaragi@gmail.com>
2026-05-17 22:32:08 +0530

    Practice

--- ExamQuestions/NthFibbonacciee.java
+++ ExamQuestions/NthFibbonacciee.java
@@ -7,7 +7,6 @@ public class NthFibbonacciee
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a = 1, b = 1, c = 0;
-
         if(n == 1 || n == 2) 
         {
             System.out.println(1);
