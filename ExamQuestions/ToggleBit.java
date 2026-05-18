3a9acbe9b4864197f99a5ec2ea2d2d0e8a7972c5
Swapna <swapnanimbaragi@gmail.com>
2026-05-18 10:59:31 +0530

    Practice

---
+++ ExamQuestions/ToggleBit.java
@@ -0,0 +1,17 @@
+package ExamQuestions;
+import java.util.*;
+public class ToggleBit {
+   public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int bits = Integer.toBinaryString(n).length();
+
+        int mask = (1 << bits) - 1;
+
+        int result = n ^ mask;
+
+        System.out.println(result);
+   }
+}
