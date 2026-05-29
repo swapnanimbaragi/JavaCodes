53180b305ff4860c34102d555b2a6c16aff36cb9
Swapna <swapnanimbaragi@gmail.com>
2026-05-29 11:21:16 +0530

    Practice

---
+++ ExamQuestions/PalendromeNum.java
@@ -0,0 +1,25 @@
+package ExamQuestions;
+import java.util.*;
+public class PalendromeNum {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        int originalNum=num;
+        int reversedNum=0;
+        while(num>0)
+        {
+            int digit=num%10;
+            reversedNum=reversedNum*10+digit;
+            num=num/10;
+        }
+        if(originalNum==reversedNum)
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
