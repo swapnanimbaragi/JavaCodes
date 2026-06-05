8c2c1af13a7304feaf8fd49fec5c14b46ee3baee
Swapna <swapnanimbaragi@gmail.com>
2026-06-05 23:31:55 +0530

    practice

--- ExamQuestions/PalendromeNum.java
+++ ExamQuestions/PalendromeNum.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class PalendromeNum {
     public static void main(String[] args)
     {
-        Scanner scan=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         int num=scan.nextInt();
         int originalNum=num;
         int reversedNum=0;
