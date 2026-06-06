de3496a31786ae4eacec8682ce133b0aeec1f37b
Swapna <swapnanimbaragi@gmail.com>
2026-06-06 22:38:30 +0530

    practice

--- ExamQuestions/CharCount.java
+++ ExamQuestions/CharCount.java
@@ -51,7 +51,7 @@ public class CharCount {
             }
             else
             {
-                System.out.print(" are Repeating Characters");
+                System.out.print("are Repeating Characters");
             }
         }
     }
