ac05c97e88f79c099a118f14a8e9a37b76a52fe3
swapnanimbaragi <swapnanimbaragi@gmail.com>
2026-06-08 22:03:27 +0530

    Fix formatting of main method in ConsecutiveSubarray

--- Array/ConsecutiveSubarray.java
+++ Array/ConsecutiveSubarray.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class ConsecutiveSubarray 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
