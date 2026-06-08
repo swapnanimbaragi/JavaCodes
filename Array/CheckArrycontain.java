f3d0236cba9835267b200963b8f2781827af9daf
swapnanimbaragi <swapnanimbaragi@gmail.com>
2026-06-08 22:01:29 +0530

    Fix formatting of main method in CheckArrycontain

--- Array/CheckArrycontain.java
+++ Array/CheckArrycontain.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class CheckArrycontain 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scan=new Scanner(System.in);
         
         int a[]=new int[5];
