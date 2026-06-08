045cd3460cd6d34df3eeeaae6e60c52b8b62610e
swapnanimbaragi <swapnanimbaragi@gmail.com>
2026-06-08 22:02:40 +0530

    Fix formatting of main method in ConsecutiveSubArrayDecending

--- Array/ConsecutiveSubArrayDecending.java
+++ Array/ConsecutiveSubArrayDecending.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class ConsecutiveSubArrayDecending 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
