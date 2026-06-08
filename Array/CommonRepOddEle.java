e56d24beb3d1e04fdd304e217a1696149d1defa5
swapnanimbaragi <swapnanimbaragi@gmail.com>
2026-06-08 22:02:07 +0530

    Fix formatting issues in CommonRepOddEle.java

--- Array/CommonRepOddEle.java
+++ Array/CommonRepOddEle.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class CommonRepOddEle {
+public class CommonRepOddEle 
+{
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
