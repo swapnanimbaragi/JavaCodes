efc141131279dff752fe2588b39ab7da6e07a9a7
Swapna <swapnanimbaragi@gmail.com>
2026-06-18 22:48:21 +0530

    Practice

--- Practice2/SumOfArrayElements.java
+++ Practice2/SumOfArrayElements.java
@@ -28,16 +28,16 @@ public class SumOfArrayElements {
         // }
         // System.out.println("Largest element in the array: "+largest);
 
-        //smallest element in the array
-        // int smallest=Integer.MAX_VALUE;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(a[i]<smallest)
-        //     {
-        //         smallest=a[i];
-        //     }
-        // }
-        // System.out.println("Smallest element in the array: "+smallest);
+        smallest element in the array
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<smallest)
+            {
+                smallest=a[i];
+            }
+        }
+        System.out.println("Smallest element in the array: "+smallest);
 
         // reverse of the array
         // for(int i=n-1;i>=0;i--)
