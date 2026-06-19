7c4f1ce1d3b5f82e8519d845fd6b032f8ad73524
Swapna <swapnanimbaragi@gmail.com>
2026-06-19 23:28:16 +0530

    Practice

--- Practice2/SumOfArrayElements.java
+++ Practice2/SumOfArrayElements.java
@@ -163,21 +163,21 @@ public class SumOfArrayElements {
         // }
         // System.out.println("It is a palindrome");
 
-        // Vowel Count
-        // String s=scanner.nextLine();
-        // int count=0;
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     char ch=s.charAt(i);
-        //     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'
-        //         || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' 
-        //     )
-        //     {
-        //         count++;
-        //     }
+        Vowel Count
+        String s=scanner.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'
+                || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' 
+            )
+            {
+                count++;
+            }
             
-        // }
-        // System.out.println(count);
+        }
+        System.out.println(count);
 
         // Word Count
         // String s=scanner.nextLine();
