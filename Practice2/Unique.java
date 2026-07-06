1ba302c9b7cfb6385fc5ffe1a26d13854d78acdf
Swapna <swapnanimbaragi@gmail.com>
2026-07-06 17:02:16 +0530

    practice

--- Practice2/Unique.java
+++ Practice2/Unique.java
@@ -1,28 +1,58 @@
-import java.util.Scanner;
+import java.util.*;
 public class Unique {
     public static void main(String args[])
     {
         Scanner scan=new Scanner(System.in);
-        String s=scan.nextLine();
+
+        // Unique character in a string
+        // String s=scan.nextLine();
+        // int count=0;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     for(int j=0;j<s.length();j++)
+        //     {
+        //         if(s.charAt(i)==s.charAt(j))
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // if(count==1)
+        // {
+        //     System.out.println("Unique");
+        // }
+        // else
+        // {
+        //     System.out.println("Not Unique");
+        // }
+
+        // Amstrong number
+        int num=scan.nextInt();
+        int temp=num;
         int count=0;
-        for(int i=0;i<s.length();i++)
+        while(temp>0)
         {
-            for(int j=0;j<s.length();j++)
-            {
-                if(s.charAt(i)==s.charAt(j))
-                {
-                    count++;
-                }
-            }
+            temp=temp/10;
+            count++;
         }
-        if(count==1)
+        temp=num;
+        int sum=0;
+        while(num>0)
         {
-            System.out.println("Unique");
+            int digits=num%10;
+            sum+=Math.pow(digits,count);
+            num=num/10;
+        }
+        if(sum==temp)
+        {
+            System.out.println("It is an Amstrong number");
         }
         else
         {
-            System.out.println("Not Unique");
+            System.out.println("It is not an Amstrong number");
         }
+
+
 
     }
     
