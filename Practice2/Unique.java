91fe940b01886009d088e2b6a707001192906b2d
Swapna <swapnanimbaragi@gmail.com>
2026-07-06 17:11:09 +0530

    practice

--- Practice2/Unique.java
+++ Practice2/Unique.java
@@ -99,6 +99,18 @@ public class Unique {
         // }
 
 
+        int n=scan.nextInt();
+        int temp=n;
+        int rev=0;
+        while(n>0)
+        {
+            int digit=n%10;
+            rev=rev*10+digit;
+            n=n/10;
+        }
+        System.out.println(rev);
+
+
 
     }
     
