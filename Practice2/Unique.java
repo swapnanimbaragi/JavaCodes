4f4c3d865b63f62cd46dbbe77d30b7a7f27fa0d2
Swapna <swapnanimbaragi@gmail.com>
2026-07-06 17:05:35 +0530

    practice

--- Practice2/Unique.java
+++ Practice2/Unique.java
@@ -52,6 +52,33 @@ public class Unique {
         //     System.out.println("It is not an Amstrong number");
         // }
 
+        // Prime Number
+        int n=scan.nextInt();
+        boolean prime=true;
+        if(n<=1)
+        {
+            prime=false;
+        }
+        else
+        {
+            for(int i=2;i<=n/2;i++)
+            {
+                if(n%i==0)
+                {
+                    prime=false;
+                    break;
+                }
+            }
+        }
+        if(prime)
+        {
+            System.out.println("It is a prime number");
+        }
+        else
+        {
+            System.out.println("It is not a prime number");
+        }
+
 
 
     }
