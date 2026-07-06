b609f53c305ea9a41ffa189320b2c135ef2efce1
Swapna <swapnanimbaragi@gmail.com>
2026-07-06 17:06:06 +0530

    practice

--- Practice2/Unique.java
+++ Practice2/Unique.java
@@ -53,31 +53,31 @@ public class Unique {
         // }
 
         // Prime Number
-        int n=scan.nextInt();
-        boolean prime=true;
-        if(n<=1)
-        {
-            prime=false;
-        }
-        else
-        {
-            for(int i=2;i<=n/2;i++)
-            {
-                if(n%i==0)
-                {
-                    prime=false;
-                    break;
-                }
-            }
-        }
-        if(prime)
-        {
-            System.out.println("It is a prime number");
-        }
-        else
-        {
-            System.out.println("It is not a prime number");
-        }
+        // int n=scan.nextInt();
+        // boolean prime=true;
+        // if(n<=1)
+        // {
+        //     prime=false;
+        // }
+        // else
+        // {
+        //     for(int i=2;i<=n/2;i++)
+        //     {
+        //         if(n%i==0)
+        //         {
+        //             prime=false;
+        //             break;
+        //         }
+        //     }
+        // }
+        // if(prime)
+        // {
+        //     System.out.println("It is a prime number");
+        // }
+        // else
+        // {
+        //     System.out.println("It is not a prime number");
+        // }
 
 
 
