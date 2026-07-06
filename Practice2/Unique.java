0b9f07ee750e5a9ce9a33b1f134f74f8533d9730
Swapna <swapnanimbaragi@gmail.com>
2026-07-06 17:11:51 +0530

    practice

--- Practice2/Unique.java
+++ Practice2/Unique.java
@@ -99,16 +99,16 @@ public class Unique {
         // }
 
         // Reverese digits of a number
-        int n=scan.nextInt();
-        int temp=n;
-        int rev=0;
-        while(n>0)
-        {
-            int digit=n%10;
-            rev=rev*10+digit;
-            n=n/10;
-        }
-        System.out.println(rev);
+        // int n=scan.nextInt();
+        // int temp=n;
+        // int rev=0;
+        // while(n>0)
+        // {
+        //     int digit=n%10;
+        //     rev=rev*10+digit;
+        //     n=n/10;
+        // }
+        // System.out.println(rev);
 
 
 
