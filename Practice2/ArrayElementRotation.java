c310a23bf636da1a97f80ca1cee40f657e6f3e21
Swapna <swapnanimbaragi@gmail.com>
2026-07-09 12:19:19 +0530

    practice

---
+++ Practice2/ArrayElementRotation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ArrayElementRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int d = scanner.nextInt();
+        for(int r=1;r<=d;r++)
+        {
+            int temp=arr[0];
+            for(int i=0;i<n-1;i++)
+            {
+                arr[i]=arr[i+1];
+            }
+            arr[n-1]=temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
